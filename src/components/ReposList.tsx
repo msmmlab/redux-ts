71edc753c8998962ba15ab6265073fa64d0cdd3d
Marcin Stepien <ms@mmlab.ie>
2021-07-11 14:07:47 +1000

    created reposList

--- src/components/ReposList.tsx
+++ src/components/ReposList.tsx
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
+import { useActions } from "../hooks/useActions";
+
 const ReposList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepos } = useActions();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    searchRepos(term);
+  };
   return (
     <div>
-      <input />
-      <button>Search</button>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
     </div>
   );
 };
