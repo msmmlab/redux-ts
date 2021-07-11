c204573da330826a28d5649bbe1685391d2df1d5
Marcin Stepien <ms@mmlab.ie>
2021-07-11 14:44:36 +1000

    reaching store from ReposList

--- src/components/ReposList.tsx
+++ src/components/ReposList.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 
 const ReposList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepos } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.repos);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -15,6 +17,10 @@ const ReposList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
